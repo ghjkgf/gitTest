cdb53b2507e3aee50984575cb309915c10a5d7a1
zsl <zhaosenlin@dbhs.com.cn>
2023-01-10 11:59:30 +0800

    222

---
+++ child/parentIsTest/src/main/java/Yo.java
@@ -0,0 +1,12 @@
+import java.sql.SQLOutput;
+
+/**
+ * @author zsl
+ * @date 2023/1/10
+ * @apiNote
+ */
+public class Yo {
+    public static void main(String[] args) {
+        System.out.println("how");
+    }
+}
